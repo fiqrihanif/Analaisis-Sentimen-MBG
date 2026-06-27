ca71842651abfd99402e91edb74ef3b3404d1f0e
fikrihanif <fikrihanif2005@gmail.com>
2026-06-27 07:23:35 +0700

    Update path model di loaders.py

--- utils/loaders.py
+++ utils/loaders.py
@@ -1,21 +1,20 @@
-import streamlit as st
+import os
 import joblib
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+import streamlit as st
 
 @st.cache_resource
 def load_all_models():
-    """Memuat file model dan TF-IDF Vectorizer"""
-    model_lr = joblib.load('D:\code\Analaisis Sentimen MBG\model\model_lr_mbg.pkl')
-    model_nb = joblib.load('D:\code\Analaisis Sentimen MBG\model\model_nb_mbg.pkl')
-    vectorizer = joblib.load(r'D:\code\Analaisis Sentimen MBG\model\tfidf_vectorizer.pkl')
+    # 1. Tentukan folder dasar tempat file ini (loaders.py) berada
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    
+    # 2. Cari folder 'model' yang berada satu tingkat di atas folder 'utils'
+    # (Jika folder 'model' sejajar dengan folder 'utils')
+    project_root = os.path.dirname(base_path)
+    model_path = os.path.join(project_root, 'model')
+    
+    # 3. Load model menggunakan path yang sudah dinamis
+    model_lr = joblib.load(os.path.join(model_path, 'model_lr_mbg.pkl'))
+    model_nb = joblib.load(os.path.join(model_path, 'model_nb_mbg.pkl'))
+    vectorizer = joblib.load(os.path.join(model_path, 'tfidf_vectorizer.pkl'))
+    
     return model_lr, model_nb, vectorizer
-
-@st.cache_resource
-def load_sastrawi():
-    """Memuat alat Stemming dan himpunan Stopwords dari Sastrawi"""
-    stemmer = StemmerFactory().create_stemmer()
-    stop_factory = StopWordRemoverFactory()
-    # Mengambil daftar stopword Sastrawi dalam bentuk set()
-    sastrawi_stopwords = set(stop_factory.get_stop_words())
-    return stemmer, sastrawi_stopwords
